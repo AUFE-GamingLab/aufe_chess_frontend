9c5419776b3a6fd3d35ca785f748800b47774fde
asyncio886 <2409298768@qq.com>
2022-11-11 20:13:48 +0800

    添加antd-vue依赖

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
+import "ant-design-vue/dist/antd.css";
+
 
 createApp(App).use(createPinia()).use(router).mount('#app')
